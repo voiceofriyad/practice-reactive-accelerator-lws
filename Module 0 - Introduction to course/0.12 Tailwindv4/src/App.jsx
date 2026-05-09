11dee48c1c23ed4052418a63b8001cad10722ea5
voiceofriyad <Contactme.riyad@yahoo.com>
2026-05-09 14:49:21 +0600

    feat: add tailwind v4

---
+++ Module 0 - Introduction to course/0.12 Tailwindv4/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-mint-500">
+        Welcome to Tailwind v4
+      </h1>
+    </div>
+  );
+}
+
+// export default App;
