8c83499e8c9b83b804fbd8a830be13c73831c49e
voiceofriyad <Contactme.riyad@yahoo.com>
2026-05-15 00:18:07 +0600

    feat: add how react works virtual dom

---
+++ Module 1 - Getting started with React Describing the UI/1.3 How React works Virtual DOM/Increment.js
@@ -0,0 +1,17 @@
+const domContainer = document.querySelector("#root");
+
+const Increment = () => {
+  const [counter, setCounter] = React.useState(0);
+  return (
+    <div>
+      <h1 id="display">{counter}</h1>
+      <div>
+        <button id="button" onClick={() => setCounter(counter + 1)}>
+          Increment +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<Increment />, domContainer);
---
+++ Module 1 - Getting started with React Describing the UI/1.3 How React works Virtual DOM/dom.js
@@ -0,0 +1,18 @@
+let array = [];
+increment = 0;
+let container = document.querySelector(".container");
+
+//fast
+
+// while (increment < 10000) {
+//   array.push(++increment);
+// }
+
+container.innerHTML = array.join(" ");
+
+//slow
+
+// while (increment < 10000) {
+//   increment++;
+//   container.innerHTML += " " + increment;
+// }
