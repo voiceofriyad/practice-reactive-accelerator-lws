1d71ce3ec072bdef29ece784b1fa30b70285c7b6
voiceofriyad <Contactme.riyad@yahoo.com>
2026-05-16 15:04:14 +0600

    fix: update Gallery file on 1.4

--- Module 1 - Getting started with React Describing the UI/1.4 - Basics of React Components Your first component/src/App.jsx
+++ Module 1 - Getting started with React Describing the UI/1.4 - Basics of React Components Your first component/src/App.jsx
@@ -1,10 +1,10 @@
-import Profile from "./components/Gallery";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <div>
       <p>Hello React</p>
-      <Profile />
+      <Gallery />
     </div>
   );
 }
