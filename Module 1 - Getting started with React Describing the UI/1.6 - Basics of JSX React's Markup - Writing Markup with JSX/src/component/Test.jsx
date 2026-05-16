ef00457fea3e8e092e6ecbc52011bcfa778d9aad
voiceofriyad <Contactme.riyad@yahoo.com>
2026-05-17 00:29:57 +0600

    feat: add Basics of JSX React's Markup - Writing Markup with JSX

---
+++ Module 1 - Getting started with React Describing the UI/1.6 - Basics of JSX React's Markup - Writing Markup with JSX/src/component/Test.jsx
@@ -0,0 +1,17 @@
+export default function Test() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://react.dev/images/docs/scientists/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        class="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
