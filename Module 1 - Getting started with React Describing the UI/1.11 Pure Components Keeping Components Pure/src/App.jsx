7a03b35a53330ba1ee9a41a898a2ce83454166df
voiceofriyad <Contactme.riyad@yahoo.com>
2026-05-31 22:44:10 +0600

    feat: adding Pure Components Keeping Components Pure

---
+++ Module 1 - Getting started with React Describing the UI/1.11 Pure Components Keeping Components Pure/src/App.jsx
@@ -0,0 +1,19 @@
+import Pani from "./components/Chai";
+import TeaGathering from "./components/TeaGathering";
+import TeaSet from "./components/TeaSet";
+import TeaSet2 from "./components/TeaSet2";
+
+function App() {
+  return (
+    <>
+      <Pani />
+      <TeaSet />
+      <p>Tea Set 2</p>
+      <TeaSet2 />
+      <p>Tea Gathering</p>
+      <TeaGathering />
+    </>
+  );
+}
+
+export default App;
